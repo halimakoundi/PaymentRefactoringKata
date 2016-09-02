a63ed1dc11bfdb3bed987d6a0d0b8bd3663d2480
Halima KOUNDI <hkoundi@gmail.com>
2016-09-02 12:34:41 +0100

    initial project set up

--- PaymentMethodRefactoring.Tests/Class1.cs
+++ PaymentMethodRefactoring.Tests/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PaymentMethodRefactoring.Src
+namespace PaymentMethodRefactoring.Tests
 {
     public class Class1
     {
