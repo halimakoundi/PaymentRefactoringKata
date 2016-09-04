fa08745e84facc514e89ec2f6617279ec8694443
Halima KOUNDI <hkoundi@gmail.com>
2016-09-04 15:26:15 +0100

    public static void ExecuteCheckPayment(Payment payment, TransactionRepo transactionRepo)

--- PaymentMethodRefactoring.Src/PaymentService.cs
+++ PaymentMethodRefactoring.Src/PaymentService.cs
@@ -20,43 +20,42 @@
             SendConfirmationEmail(customerId, orderId, paymentMethod);
         }
 
-        public class Payment
-        {
-            public Payment(decimal amount, string orderId, string paymentMethod)
-            {
-                Amount = amount;
-                OrderId = orderId;
-                PaymentMethod = paymentMethod;
-            }
-
-            public decimal Amount { get; }
-
-            public string OrderId { get; }
-
-            public string PaymentMethod { get; }
-        }
-
         private void ExecutePayment(Payment payment)
         {
             switch (payment.PaymentMethod)
             {
                 case "check":
-                    var cashTransaction = PaymentTransaction.With(payment.PaymentMethod, payment.Amount, payment.OrderId);
-                    _transactionRepo.Save(cashTransaction);
+                    ExecuteCheckPayment(payment, _transactionRepo);
                     break;
                 case "card":
-                    _paymentProvider.AuthorisePayment(payment.Amount, payment.OrderId, payment.PaymentMethod);
-
-                    var cardTransaction = PaymentTransaction.With(payment.PaymentMethod, payment.Amount, payment.OrderId);
-                    _transactionRepo.Save(cardTransaction);
+                    ExecuteCardPayment(payment, _paymentProvider, _transactionRepo);
                     break;
                 case "direct-debit":
-                    _paymentProvider.AuthorisePayment(payment.Amount, payment.OrderId, payment.PaymentMethod);
-
-                    var directDebitTransaction = PaymentTransaction.With(payment.PaymentMethod, payment.Amount, payment.OrderId);
-                    _transactionRepo.Save(directDebitTransaction);
+                    ExecuteDirectDebitPayment(payment);
                     break;
             }
+        }
+
+        public static void ExecuteCheckPayment(Payment payment, TransactionRepo transactionRepo)
+        {
+            var cashTransaction = PaymentTransaction.With(payment.PaymentMethod, payment.Amount, payment.OrderId);
+            transactionRepo.Save(cashTransaction);
+        }
+
+        private void ExecuteCardPayment(Payment payment, IPaymentProvider paymentProvider, TransactionRepo transactionRepo)
+        {
+            paymentProvider.AuthorisePayment(payment.Amount, payment.OrderId, payment.PaymentMethod);
+
+            var cardTransaction = PaymentTransaction.With(payment.PaymentMethod, payment.Amount, payment.OrderId);
+            transactionRepo.Save(cardTransaction);
+        }
+
+        private void ExecuteDirectDebitPayment(Payment payment)
+        {
+            _paymentProvider.AuthorisePayment(payment.Amount, payment.OrderId, payment.PaymentMethod);
+
+            var directDebitTransaction = PaymentTransaction.With(payment.PaymentMethod, payment.Amount, payment.OrderId);
+            _transactionRepo.Save(directDebitTransaction);
         }
 
         private void SendConfirmationEmail(string customerId, string orderId, string paymentMethod)
@@ -64,5 +63,21 @@
             var orderConfirmationEmail = _emailGateway.NewEmailFor(orderId, customerId, paymentMethod);
             _emailGateway.Send(orderConfirmationEmail);
         }
+    }
+
+    public class Payment
+    {
+        public Payment(decimal amount, string orderId, string paymentMethod)
+        {
+            Amount = amount;
+            OrderId = orderId;
+            PaymentMethod = paymentMethod;
+        }
+
+        public decimal Amount { get; }
+
+        public string OrderId { get; }
+
+        public string PaymentMethod { get; }
     }
 }
