0a10933580de63ab9ae44ba7c9e9760f31a4e613
Halima KOUNDI <hkoundi@gmail.com>
2016-09-04 11:20:09 +0100

    refactoring unit test into test case

--- PaymentMethodRefactoring.Tests/PaymentServiceShould.cs
+++ PaymentMethodRefactoring.Tests/PaymentServiceShould.cs
@@ -7,25 +7,28 @@ namespace PaymentMethodRefactoring.Tests
     [TestFixture]
     public class PaymentServiceShould
     {
-        private const string OrderId = "123-324-456";
         private PaymentService _paymentService;
         private PaymentProvider _paymentProvider;
         private IEmailGateway _emailGateway;
 
-        [Test]
-        public void pay_order_and_send_confirmation_email()
+        [SetUp]
+        public void SetUp()
         {
-            Substitute.For<UserAccountChecker>();
             _paymentProvider = Substitute.For<PaymentProvider>();
             _emailGateway = Substitute.For<IEmailGateway>();
             _paymentService = new PaymentService(_paymentProvider, _emailGateway);
-            var paymentMethod = "card";
-            Email email = new OrderConfirmationEmail(OrderId, paymentMethod);
-            var paymentInfo = new PaymentInfo();
-            _emailGateway.NewEmailFor(OrderId, paymentMethod).Returns(email);
-            _paymentService.Pay(OrderId, paymentMethod, paymentInfo);
+        }
 
-            _paymentProvider.Received().MakePayment(OrderId, paymentInfo);
+        [TestCase("card", "123-324-456")]
+        public void pay_order_and_send_confirmation_email(string paymentMethod, string orderId)
+        {
+            var email = new OrderConfirmationEmail(orderId, paymentMethod);
+            var paymentInfo = new PaymentInfo();
+            _emailGateway.NewEmailFor(orderId, paymentMethod).Returns(email);
+
+            _paymentService.Pay(orderId, paymentMethod, paymentInfo);
+
+            _paymentProvider.Received().MakePayment(orderId, paymentInfo);
             _emailGateway.Received().Send(email);
         }
     }
