06ea5f114ef65e50e5c53fc523d53899e2c5c112
Halima KOUNDI <hkoundi@gmail.com>
2016-09-04 12:24:06 +0100

    remove comment

--- PaymentMethodRefactoring.Src/PaymentService.cs
+++ PaymentMethodRefactoring.Src/PaymentService.cs
@@ -35,7 +35,6 @@
                     break;
             }
 
-            //TODO here we will extract method below into sendConfirmationEmail method
             var orderConfirmationEmail  = _emailGateway.NewEmailFor(orderId, customerId, paymentMethod);
             _emailGateway.Send(orderConfirmationEmail);
         }
