3e47df0dffd5d6a1378c33dfab4feeaee324b21d
Halima KOUNDI <hkoundi@gmail.com>
2016-09-04 15:06:33 +0100

    extract method class payment details from parameter

--- PaymentMethodRefactoring.Src/PaymentService.cs
+++ PaymentMethodRefactoring.Src/PaymentService.cs
@@ -15,29 +15,45 @@
 
         public void Pay(decimal amount, string customerId, string orderId, string paymentMethod)
         {
-            ExecutePayment(amount, orderId, paymentMethod);
+            ExecutePayment(new PaymentDetails(amount, orderId, paymentMethod));
 
             SendConfirmationEmail(customerId, orderId, paymentMethod);
         }
 
-        private void ExecutePayment(decimal amount, string orderId, string paymentMethod)
+        public class PaymentDetails
         {
-            switch (paymentMethod)
+            public PaymentDetails(decimal amount, string orderId, string paymentMethod)
+            {
+                Amount = amount;
+                OrderId = orderId;
+                PaymentMethod = paymentMethod;
+            }
+
+            public decimal Amount { get; }
+
+            public string OrderId { get; }
+
+            public string PaymentMethod { get; }
+        }
+
+        private void ExecutePayment(PaymentDetails paymentDetails)
+        {
+            switch (paymentDetails.PaymentMethod)
             {
                 case "check":
-                    var cashTransaction = PaymentTransaction.With(paymentMethod, amount, orderId);
+                    var cashTransaction = PaymentTransaction.With(paymentDetails.PaymentMethod, paymentDetails.Amount, paymentDetails.OrderId);
                     _transactionRepo.Save(cashTransaction);
                     break;
                 case "card":
-                    _paymentProvider.AuthorisePayment(amount, orderId, paymentMethod);
+                    _paymentProvider.AuthorisePayment(paymentDetails.Amount, paymentDetails.OrderId, paymentDetails.PaymentMethod);
 
-                    var cardTransaction = PaymentTransaction.With(paymentMethod, amount, orderId);
+                    var cardTransaction = PaymentTransaction.With(paymentDetails.PaymentMethod, paymentDetails.Amount, paymentDetails.OrderId);
                     _transactionRepo.Save(cardTransaction);
                     break;
                 case "direct-debit":
-                    _paymentProvider.AuthorisePayment(amount, orderId, paymentMethod);
+                    _paymentProvider.AuthorisePayment(paymentDetails.Amount, paymentDetails.OrderId, paymentDetails.PaymentMethod);
 
-                    var directDebitTransaction = PaymentTransaction.With(paymentMethod, amount, orderId);
+                    var directDebitTransaction = PaymentTransaction.With(paymentDetails.PaymentMethod, paymentDetails.Amount, paymentDetails.OrderId);
                     _transactionRepo.Save(directDebitTransaction);
                     break;
             }
